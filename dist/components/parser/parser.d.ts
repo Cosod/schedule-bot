ea1b524778eb22e6ff2148d33941e06263aeac32
Daniil Sobyanin <sobdan69@gmail.com>
2023-11-24 10:48:17 -0800

    linux

--- dist/components/parser/parser.d.ts
+++
@@ -1,7 +0,0 @@
-import { Page } from "puppeteer";
-import { IParser } from "./parser.interface";
-export declare class Parser implements IParser {
-    content: string[][] | null;
-    constructor();
-    start(page: Page): Promise<void>;
-}
